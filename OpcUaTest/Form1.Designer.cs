597453fde06235cccab26b76a5349d08a37a5c9b
dathlin <hsl200909@163.com>
2018-01-16 22:11:34 +0800

    添加批量读取，写入的测试代码

--- OpcUaTest/Form1.Designer.cs
+++ OpcUaTest/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -63,11 +65,33 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(129, 316);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(251, 368);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(899, 513);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -84,6 +108,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- OpcUaTest/Form1.cs
+++ OpcUaTest/Form1.cs
@@ -48,9 +48,83 @@ namespace OpcUaTest
             opcUaClient.Disconnect();
         }
 
+        // ns=2;s=Robots/RobotA/RobotMode
+        // ns=2;s=Robots/RobotA/UserFloat
+
         private void button3_Click(object sender, EventArgs e)
         {
+            Opc.Ua.DataValue value = opcUaClient.ReadNode("ns=2;s=Robots/RobotA/RobotMode");
+            // 一个数据的类型是不是数组由 value.WrappedValue.TypeInfo.ValueRank 来决定的
+            // -1 说明是一个数值
+            // 1  说明是一维数组，如果类型BuiltInType是Int32，那么实际是int[]
+            // 2  说明是二维数组，如果类型BuiltInType是Int32，那么实际是int[,]
+            if (value.WrappedValue.TypeInfo.BuiltInType == Opc.Ua.BuiltInType.Int32)
+            {
+                if (value.WrappedValue.TypeInfo.ValueRank == -1)
+                {
+                    int temp = (int)value.WrappedValue.Value;               // 最终值
+                }
+                else if (value.WrappedValue.TypeInfo.ValueRank == 1)
+                {
+                    int[] temp = (int[])value.WrappedValue.Value;           // 最终值
+                }
+                else if (value.WrappedValue.TypeInfo.ValueRank == 2)
+                {
+                    int[,] temp = (int[,])value.WrappedValue.Value;         // 最终值
+                }
+            }
+            else if(value.WrappedValue.TypeInfo.BuiltInType == Opc.Ua.BuiltInType.UInt32)
+            {
+                uint temp = (uint)value.WrappedValue.Value;                 // 数组的情况参照上面的例子
+            }
+            else if (value.WrappedValue.TypeInfo.BuiltInType == Opc.Ua.BuiltInType.Float)
+            {
+                float temp = (float)value.WrappedValue.Value;               // 数组的情况参照上面的例子
+            }
+            else if (value.WrappedValue.TypeInfo.BuiltInType == Opc.Ua.BuiltInType.String)
+            {
+                string temp = (string)value.WrappedValue.Value;             // 数组的情况参照上面的例子
+            }
+            else if (value.WrappedValue.TypeInfo.BuiltInType == Opc.Ua.BuiltInType.DateTime)
+            {
+                DateTime temp = (DateTime)value.WrappedValue.Value;         // 数组的情况参照上面的例子
+            }
+        }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[] nodes = new string[]
+            {
+                "ns=2;s=Robots/RobotA/RobotMode",
+                "ns=2;s=Robots/RobotA/UserFloat"
+            };
+
+            // 因为不能保证读取的节点类型一致，所以只提供统一的DataValue读取，每个节点需要单独解析
+            foreach(Opc.Ua.DataValue value in opcUaClient.ReadNodes(nodes))
+            {
+                // 获取到了值，具体的每个变量的解析参照上面类型不确定的解析
+                object data = value.WrappedValue.Value;
+                // 下面写你自己的操作
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // 批量写入的代码
+            string[] nodes = new string[]
+            {
+                "ns=2;s=Robots/RobotA/RobotMode",
+                "ns=2;s=Robots/RobotA/UserFloat"
+            };
+            object[] data = new object[]
+            {
+                4,
+                new float[]{5,3,1,5,7,8}
+            };
+
+            // 都成功返回True，否则返回False
+            bool result = opcUaClient.WriteNodes(nodes, data);
         }
     }
 }
